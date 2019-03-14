4d216cbc77232a696b75a32115f1121b99533bea
anton <anton@192.168.0.3>
2019-03-14 16:01:39 +0100

    på,l.

--- Hermes/src/system/UserFactory.java
+++ Hermes/src/system/UserFactory.java
@@ -1,23 +1,48 @@
 package system;
 
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 public class UserFactory {
-	
-	private String userName;
-	
-	public UserFactory(String userName){
-		this.userName = userName;
+
+	public UserFactory(){
+
 	}
-	
-	public User createUser(){
-		User theUser = new User(this.userName);
-		return theUser;
-	}
-	
-	public int getUserType(){
-		
-		DBConnection DB = new DBConnection();
+
+	//connecta till db
+	public User getUserType(String userName) throws SQLException{
+
+		DBConnection DB = new DBConnection(userName);
 		return DB.getEmployeeType(); // 1=Worker, 2=ProjectManager & 3=DepartmentManager
 	}
-	
-	
+
+	public <E> E createUser(String userName, int userType){
+		if (userType == 1){
+			Worker w = new Worker(userName);
+			return (E) w;
+		}
+		else if (userType == 2){
+			ProjectManager pm = new ProjectManager(userName);
+			return (E) pm;
+		}
+		else if (userType == 3){
+			DepartmentManager dm = new DepartmentManager(userName);
+			return (E) dm;
+		}
+		else{
+			//Ska tas bort innan vi skickar in
+			JOptionPane.showInputDialog("ERROR, ange userType 1, 2 eller 3 Förfan! Usern sätts som Worker nu istället");
+			Worker standardUser = new Worker(userName);		
+			return (E) standardUser;
+
+			}
+		
+		
+		
+	}
+
+
+
+
 }
