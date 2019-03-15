86028049d69ed7bae1d89b423b308db9190b277c
Justad <Justad@host-193-11-185-220.eduroam.bth.se>
2019-03-15 10:26:15 +0100

    first test of login

--- Hermes/src/system/DBConnection.java
+++ Hermes/src/system/DBConnection.java
@@ -1,10 +1,6 @@
 package system;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DBConnection {
 
@@ -53,7 +49,15 @@ public class DBConnection {
 	
 	public String getPW(String userName) throws SQLException {
 		
-		String password = String.valueOf(myConn.prepareStatement("SELECT * FROM tabelNamn WHERE userName = this.userName "));
+//		String password = String.valueOf(myConn.prepareStatement("SELECT * FROM tabelNamn WHERE userName = this.userName "));
+		PreparedStatement preMyStmt = myConn.prepareStatement("Select password FROM login WHERE username = ?");
+		preMyStmt.setString(1, userName);
+		
+		ResultSet myRs = preMyStmt.executeQuery();
+		
+		String password = myRs.getString("password");
+		
+		
 		
 		return password;
 		
