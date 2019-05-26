080d5ad506cd0c1efbdb7426643101a131755e93
Wide <Antebooy@gmail.com>
2019-05-26 22:43:17 +0200

    stor dase

--- Hermes/src/refactor/AddOrEditActivityToProjectController.java
+++ Hermes/src/refactor/AddOrEditActivityToProjectController.java
@@ -1,18 +1,47 @@
 package refactor;
 
+import java.awt.Button;
+import javafx.scene.layout.Pane;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
 import systemFixPackage.timeReport;
 
 public class AddOrEditActivityToProjectController {
 	private MenuGuiController mainC;
 	private timeReport timeReporter = new timeReport();
-
+	
+	@FXML
+	private Button addActivity;
+	@FXML
+	private Button editActivity;
+	
+	@FXML
+	private Pane hiddenPane;
+	
+	
+	
 	public void injectMainController(MenuGuiController mainC){
 		this.mainC = mainC;
 		System.out.println("This message shows us that the controller is set up (AddOrEditActivityToProject)");
+		hiddenPane.setVisible(false);
+	}
+	
+	private void addActivity(ActionEvent event) {
+		hiddenPane.setVisible(true);
+		
+		
+		
+	}
+	
+	private void editActivity(ActionEvent event) {
+		
+		
 	}
 
 	/*
 	 * Your code should be below this
 	 */
+	
 	
 }
